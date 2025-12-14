a922bfa2e559c8f55f6163eb429032680704794a
redgeography <128873224+redgeography@users.noreply.github.com>
2025-12-14 09:40:26 -0500

    Update blocks.js

--- src/blocks.js
+++ src/blocks.js
@@ -857,7 +857,7 @@ SyntaxElementMorph.prototype.labelParts = {
         type: 'input',
         tags: 'read-only static',
         menu: {
-			any: ["any\n"],
+			any: "any",
 			all: ["all"]
 		}
     },
